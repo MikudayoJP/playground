37af04e8561a8ff4f7efb2c9d5a6d3b6d4d7f205
Mikudayo <mikudayojp@gmail.com>
2024-02-16 00:48:41 +0100

    added function to close game to player.gd

--- scripts/player.gd
+++ scripts/player.gd
@@ -81,7 +81,11 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
+	
+	# closes game
+	if Input.is_action_just_pressed("open_menu"):
+		get_tree().quit()
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
