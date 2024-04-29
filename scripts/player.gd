3ba43123f03daa99d142331c01419c04c52b71ed
Mikudayo <mikudayojp@gmail.com>
2024-04-29 20:42:18 +0200

    added asset docker, added background and added sprinting

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var current_speed = 300.0
+@export var jump_velocity = -400.0
+@export var walking_speed = 300.0
+@export var sprinting_speed = 1000.0
+@export var lerp_speed = 10.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -15,17 +18,22 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * current_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, current_speed)
 		
 	if Input.is_action_just_pressed("debug_close"):
-			get_tree().quit()
+		get_tree().quit()
+	
+	if Input.is_action_pressed("sprint"):
+		current_speed = sprinting_speed
+	else:
+		current_speed = walking_speed
 	
 	move_and_slide()
