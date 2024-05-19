6c8a18a7d843bdad033ef143c859a52b3429d8fa
Mikudayo <mikudayojp@gmail.com>
2024-05-19 22:51:10 +0200

    added a deactivation for camera trigger when activated once (and put a timer before setting bool to true)

--- scripts/trigger.gd
+++ scripts/trigger.gd
@@ -3,12 +3,17 @@ extends Node2D
 @onready var colorRect = $"Area2D/ColorRect"
 @onready var pcam_2d : PhantomCamera2D = $"/root/testScene/zoom out"
 
+var cameraWasActivated : bool = false
+
 func _on_area_2d_body_entered(body):
 	#needs to be updated for player specific --done
 	#TODO needs to be put into another script
 	if body.is_in_group("Player"): #Dand Input.is_action_pressed("interact"):
 		colorRect.set_color(Color.CHARTREUSE)
-		pcam_2d.set_priority(2)
+		if cameraWasActivated == false:
+			pcam_2d.set_priority(2)
+			await get_tree().create_timer(5.0).timeout
+			cameraWasActivated = true
 	#if Input.is_action_just_pressed("interact"):
 	#	pass
 		#dprint("wadad")
